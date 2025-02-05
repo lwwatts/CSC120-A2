e0d16e490efd0c7888e5db2e798865a52d22cf57
lwwatts <lwwatts@smith.edu>
2025-02-05 10:34:21 -0500

    Filled out classes (untested)

--- computer.py
+++ computer.py
@@ -1,10 +1,35 @@
 class Computer:
 
     # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, desc: str, pro_type: str, cap: int, mem: int, os: str, year: int, val: int):
+        self.description = desc
+        self.processor_type = pro_type
+        self.hard_drive_capacity = cap
+        self.memory = mem
+        self.operating_system = os
+        self.year_made = year
+        self.price = val
 
     # What methods will you need?
+    def update_os(self, new_os: str):
+        self.operating_system = new_os
+
+    def update_price(self, new_price: int):
+        self.price = new_price
+
+    def get_year_made(self):
+        return self.year_made
+    
+    def print_computer(self):
+        print(f"Description: {self.description}, Processor type: {self.processor_type}, Hard drive capacity: {self.hard_drive_capacity},
+              Memory: {self.memory}, Operating system: {self.operating_system}, Year made: {self.year_made}, Price: ${self.price}")
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,46 @@
 class ResaleShop:
 
+    import computer
+
     # What attributes will it need?
+    inventory: list = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self, starter_inventory: list = []):
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    def buy(self, new_comp: computer):
+        self.inventory.append(new_comp)
+
+    def sell(self, item_id: int):
+        if self.inventory[item_id] is not None:
+            self.inventory.pop(item_id)
+            print("Item", item_id, "sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.")
+
+    def refurbish(self, item_id: int, new_os: str = None):
+        if self.inventory[item_id] is not None:
+            comp = self.inventory[item_id]
+            year_made = comp.get_year_made()
+            if year_made < 2000:
+                comp.update_price(0)
+            elif year_made < 2012:
+                comp.update_price(250)
+            elif year_made < 2018:
+                comp.update_price(550)
+            else:
+                comp.update_price(1000)
+            
+            if new_os is not None:
+                comp.update_os(new_os)
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+
+    def print_inventory(self):
+        if len(self.inventory) != 0:
+            for item in self.inventory:
+                print(f'Item ID: {self.inventory.index(item)} : {item}')
+
